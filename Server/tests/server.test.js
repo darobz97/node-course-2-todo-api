bf8b0fa132cc4f2578d6ffc6943162746a06bf7d
Roberto Manzanaro <robertomanzanaro97@hotmail.com>
2018-04-30 19:47:14 +0200

    Add tests for GET /todos

--- Server/tests/server.test.js
+++ Server/tests/server.test.js
@@ -5,10 +5,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+  {text: 'First test todo'},
+  {text: 'Second test todo'}
+]
+
 //This func runs before each test case and
 //only moves to the test when we call done
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 //decribe block to group all of the routes
@@ -29,7 +36,7 @@ describe('POST /todos', () => {
         return done(err);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find({text: 'Test todo text'}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -48,9 +55,21 @@ describe('POST /todos', () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     })
   })
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
 });
