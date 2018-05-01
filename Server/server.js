bba04a6bda0aea4b2edcdfc8db8f1e4c299ad9f8
Roberto Manzanaro <robertomanzanaro97@hotmail.com>
2018-05-01 17:21:03 +0200

    setup app for Heroku and taking out errors

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = provess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 //This var is going to store our express app
 var app = express();
